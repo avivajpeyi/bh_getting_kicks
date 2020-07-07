51f3a8b3b22221746948408d744e04207cb21473
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-07-08 00:07:09 +1000

    reduce time

--- plot_multiple.py
+++ plot_multiple.py
@@ -17,21 +17,24 @@ FILE = "/fred/oz117/avajpeyi/projects/phase-marginalisation-test/jobs/out_hundre
 def main():
     truths = pd.read_csv('datafiles/injections.csv')
     for index, truth in tqdm.tqdm(truths.iterrows(), total=len(truths)):
-        f = FILE.format(num=index)
-        kick_mean = truth['remnant_kick_mag']
-        kick_sigma = 50
+        if index < 45:
+            pass
+        else:
+            f = FILE.format(num=index)
+            kick_mean = truth['remnant_kick_mag']
+            kick_sigma = 50
 
-        samples = plot_corner_weighted_with_kick.Samples(
-            samples_csv=f,
-            kick_mean=kick_mean,
-            kick_sigma=kick_sigma,
-            truths=truth.to_dict()
-        )
-        try:
-            plot_corner_weighted_with_kick.process_sample(samples, kick_mean,
-                                                          kick_sigma, f)
-        except Exception as e:
-            print(f"Error: {e}")
+            samples = plot_corner_weighted_with_kick.Samples(
+                samples_csv=f,
+                kick_mean=kick_mean,
+                kick_sigma=kick_sigma,
+                truths=truth.to_dict()
+            )
+            try:
+                plot_corner_weighted_with_kick.process_sample(samples, kick_mean,
+                                                              kick_sigma, f)
+            except Exception as e:
+                print(f"Error: {e}")
 
 
 if __name__ == "__main__":
