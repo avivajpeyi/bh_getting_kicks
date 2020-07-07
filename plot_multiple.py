55ef9e6a4d8a5caede1ad8751e62e1ed02d6d0be
Avi Vajpeyi <avi.vajpeyi@gmail.com>
2020-07-07 23:28:45 +1000

    adjust plotting

--- plot_multiple.py
+++ plot_multiple.py
@@ -6,12 +6,10 @@ This module does what....
 Example usage:
 
 """
-import numpy as np
-import matplotlib.pyplot as plt
-import plot_corner_weighted_with_kick
-import tqdm
-import argparse
 import pandas as pd
+import tqdm
+
+import plot_corner_weighted_with_kick
 
 FILE = "/fred/oz117/avajpeyi/projects/phase-marginalisation-test/jobs/out_hundred_injections_gstar/out_injection_{num}/result/injection_{num}_0_posterior_samples_with_kicks.dat"
 
@@ -29,28 +27,11 @@ def main():
             kick_sigma=kick_sigma,
             truths=truth.to_dict()
         )
-        fname1 = f.replace(".dat", "_no_reweighting_corner.png")
-        samples.plot_corner(f=fname1, title="No Reweigting")
-
-        fname2 = f.replace(
-            ".dat",
-            f"_kick_mu{int(kick_mean)}_sigma{int(kick_sigma)}_corner.png"
-        )
-        samples.plot_corner(
-            f=fname2, weights=True,
-            title=r"Reweighted with {}".format(
-                plot_corner_weighted_with_kick.get_normal_string(
-                    int(kick_mean),
-                    int(kick_sigma)
-                )
-            ))
-
-        fname3 = f.replace(".dat", "_overlaid.png")
-        samples.plot_overlaid_corner(fname3)
-
-        fname4 = f.replace(".dat", "_corner.png")
-        plot_corner_weighted_with_kick.combine_images_horizontally(
-            [fname3, fname1, fname2], f=fname4)
+        try:
+            plot_corner_weighted_with_kick.process_sample(samples, kick_mean,
+                                                          kick_sigma, f)
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 if __name__ == "__main__":
